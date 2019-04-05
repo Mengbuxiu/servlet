4d9c49f09e39a36fbb62fff82422e79989529488
milu <15910344021@163.com>
2019-04-05 21:20:57 +0800

    jsp

--- src/main/java/MyServlet.java
+++ src/main/java/MyServlet.java
@@ -1,11 +1,9 @@
-import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Enumeration;
 
 /**
  * @author zhenglin
@@ -15,25 +13,16 @@ import java.util.Enumeration;
 public class MyServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+        //super.doGet(req, resp);
         System.out.println("this is get request");
-        String sessionId = req.getRequestedSessionId();
-        System.out.println(sessionId);
-        ServletConfig servletConfig = this.getServletConfig();
-        System.out.println(servletConfig.getServletName());
-        Enumeration initParameterNames = servletConfig.getInitParameterNames();
-        while (initParameterNames.hasMoreElements()){
-            System.out.println(initParameterNames.toString());
-        }
-        System.out.println(servletConfig.getServletContext().toString());
-        System.out.println(this.getServletInfo());
-        // req.getRequestDispatcher("success.jsp").forward(req,resp);
-        resp.sendRedirect("success.jsp");
+        req.setAttribute("name","zhenglin");
+        req.getRequestDispatcher("success.jsp").forward(req,resp);
+//        resp.sendRedirect("success.jsp");
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+        //super.doPost(req, resp);
         System.out.println("this is post request");
     }
 }
--- src/main/webapp/success.jsp
+++ src/main/webapp/success.jsp
@@ -5,12 +5,22 @@
   Time: 23:12
   To change this template use File | Settings | File Templates.
 --%>
-<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ page contentType="text/html;charset=UTF-8" import="java.util.*"  language="java" %>
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <html>
 <head>
     <title>Title</title>
 </head>
 <body>
     <h1>success!</h1>
+    <%--<c:if test="${not empty name}">--%>
+    <input value="<% Object name = request.getAttribute("name");
+        System.out.println(name);
+    %>">
+<%--
+    </c:if>
+--%>
+    <script src="https://cdn.jsdelivr.net/npm/jquery@1.12.4/dist/jquery.min.js"></script>
 </body>
 </html>
